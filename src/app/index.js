f6ba31ca128917ed60f40444f8d0a83d704a88fc
dima home <dimanok88@gmail.com>
2016-11-29 00:01:47 +0300

    change state

--- src/app/index.js
+++ src/app/index.js
@@ -19,9 +19,7 @@ class App extends React.Component {
                  </div>
                  <div className="row">
                      <div className="col-xs-10 col-xs-offset-1">
-                         <Home name={"Max"} age={28} user={user}>
-                             <p>This is a paragraph</p>
-                         </Home>
+                         <Home name={"Max"} initialAge={28}/>
                      </div>
                  </div>
              </div>
