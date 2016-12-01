64ac26337be286db187af4844fd952ae6ad80bce
dima <dimanok88@gmail.com>
2016-12-01 16:29:11 +0300

    component methods

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -6,12 +6,46 @@ export class Home extends React.Component {
         this.state = {
             age: props.initialAge,
             status: 0,
-            homeLink: "Change Link"
+            homeLink: props.initiaLinkName
         }
         setTimeout(()=>{
             this.setState({status: 1});
         },3000);
 
+        console.log("Constructor");
+    }
+
+    componentWillMount(){
+        console.log("Component will mount");
+    }
+    
+    componentWillUnmount(){
+        console.log("Component will unmount");
+    }
+    
+    
+    componentDidMount(){
+        console.log("Component did mount");
+    }
+    
+    componentWillReceiveProps(nextProps){
+        console.log("Component will receive props", nextProps);
+    }
+    
+    shouldComponentUpdate(nextProps, nextState){
+        console.log("Should component update", nextProps, nextState);
+        //if(nextState.status === 1) {
+        //    return false;
+        //}
+        return true;
+    }
+    
+    componentWillUpdate(nextProps, nextState){
+        console.log("Component will update", nextProps, nextState);
+    }
+    
+    componentDidUpdate(prevProps, prevState){
+        console.log("Component did update", prevProps, prevState);
     }
 
     onChangeLink(){
@@ -22,6 +56,12 @@ export class Home extends React.Component {
         this.setState({
             age: this.state.age+2
         });
+    }
+
+    onHandlerChange(event){
+        this.setState({
+            homeLink: event.target.value
+        })
     }
 
     render () {
@@ -35,6 +75,7 @@ export class Home extends React.Component {
                 <hr/>
                 <button className="btn btn-primary" onClick={this.props.greet}>Greet</button>
                 <hr/>
+                <input type="text" value={this.state.homeLink} onChange={(event) => this.onHandlerChange(event)}/>
                 <button className="btn btn-primary" onClick={this.onChangeLink.bind(this)}>Change Header Link</button>
             </div>
         );
@@ -43,6 +84,7 @@ export class Home extends React.Component {
 
 Home.propTypes = {
     name: React.PropTypes.string,
+    initialLinkName: React.PropTypes.string,
     initialAge: React.PropTypes.number,
     greet: React.PropTypes.func
 };
--- src/app/index.js
+++ src/app/index.js
@@ -8,20 +8,37 @@ class App extends React.Component {
     constructor(props){
         super();
         this.state = {
-            homeLink: "Home"
+            homeLink: "Home",
+            homeMounted: true
         }
-
     }
+
     onGreet(){
         alert("HEllo");
     }
+
     onChangeLinkName(newName){
         this.setState({
             homeLink: newName
         });
     }
+    onChangeHomeMounted(){
+        this.setState({
+            homeMounted: !this.state.homeMounted
+        });
+    }
     render() {
-         return (
+        let homeCmp = "";
+        if(this.state.homeMounted){
+            homeCmp = (<Home
+                name={"Max"}
+                initialAge={28}
+                greet={this.onGreet}
+                changeLink={this.onChangeLinkName.bind(this)}
+                initiaLinkName={this.state.homeLink}
+            />);
+        }
+        return (
              <div className="container">
                  <div className="row">
                      <div className="col-xs-10 col-xs-offset-1">
@@ -30,12 +47,12 @@ class App extends React.Component {
                  </div>
                  <div className="row">
                      <div className="col-xs-10 col-xs-offset-1">
-                         <Home
-                             name={"Max"}
-                             initialAge={28}
-                             greet={this.onGreet}
-                             changeLink={this.onChangeLinkName.bind(this)}
-                         />
+                         {homeCmp}
+                     </div>
+                 </div>
+                 <div className="row">
+                     <div className="col-xs-10 col-xs-offset-1">
+                         <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(Un)Mount Home Component</button>
                      </div>
                  </div>
              </div>
